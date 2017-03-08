0bf0e3d07b4a23230b327692f471de1a74308a65
karl-arreola <karreola@my.bcit.ca>
2017-03-08 12:34:21 -0800

    added caboose package and implemented to views, base controller

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -39,7 +39,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |  $autoload['packages'] = array(APPPATH.'third_party', '/usr/local/shared');
 |
 */
-$autoload['packages'] = array();
+$autoload['packages'] = array(APPPATH.'third_party/caboose/');
 
 /*
 | -------------------------------------------------------------------
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('parser','database');
+$autoload['libraries'] = array('parser','database','caboose','parsedown','session');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url');
+$autoload['helper'] = array('url','formfields');
 
 /*
 | -------------------------------------------------------------------
@@ -133,4 +133,3 @@ $autoload['language'] = array();
 |	$autoload['model'] = array('first_model' => 'first');
 */
 $autoload['model'] = array('flags', 'groups', 'priorities', 'sizes', 'statuses', 'tasks');
-$autoload['libraries'] = array('parser', 'database', 'parsedown','session');
--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -16,7 +16,6 @@ class Application extends CI_Controller
 	 * Constructor.
 	 * Establish view parameters & load common helpers
 	 */
-
 	function __construct()
 	{
 		parent::__construct();
@@ -25,6 +24,8 @@ class Application extends CI_Controller
 		$this->data = array ();
 		$this->data['pagetitle'] = 'TODO List Manager';
 		$this->data['ci_version'] = (ENVIRONMENT === 'development') ? 'CodeIgniter Version <strong>'.CI_VERSION.'</strong>' : '';
+		$this->data['alerts'] = '';
+    	$this->error_free = TRUE;
 	}
 
 	/**
@@ -32,13 +33,24 @@ class Application extends CI_Controller
 	*/
 	function render($template = 'template')
 	{
-		 $this->data['menubar'] = $this->parser->parse('_menubar', $this->config->item('menu_choices'),true);
-		 // use layout content if provided
-		 if (!isset($this->data['content'])) {
-			 $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
-		 }
+		$this->data['menubar'] = $this->parser->parse('_menubar', $this->config->item('menu_choices'),true);
+		// use layout content if provided
+		if (!isset($this->data['content'])) {
+			$this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
+		}
 
-		 $this->parser->parse($template, $this->data);
+		$this->data['caboose_styles'] = $this->caboose->styles();
+	   	$this->data['caboose_scripts'] = $this->caboose->scripts();
+	   	$this->data['caboose_trailings'] = $this->caboose->trailings();
+
+		$this->parser->parse($template, $this->data);
 	}
 
+	// Add an alert to the rendered page
+	function alert($message = '', $context = 'success')
+	{
+	    $parms = ['message' => $message, 'context' => $context];
+	    $this->data['alerts'] .= $this->parser->parse('_alert', $parms, true);
+	    $this->error_free = FALSE;
+	}
 }
